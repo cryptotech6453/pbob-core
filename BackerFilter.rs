cb6a63094b41b3cabe8ad56c7f6ed6c30120d9e5
cryptotech6453 <dl6453@gmail.com>
2025-03-08 16:21:09 +0200

    Update BackerFilter.rs

--- BackerFilter.rs
+++ BackerFilter.rs
@@ -1,4 +1,13 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct BackerFilter {
+    pub min_abi: u32,  // Минимальный ABI
+    pub min_games: u32,  // Минимальное количество турниров
+    pub min_games_per_month: u32,  // Минимум турниров в месяц
+    pub min_roi: f64,  // Минимальный ROI
+    pub min_dollars_per_tournament: f64, // Минимальный выигрыш с турнира
+    pub afs_range: (u32, u32),  // Диапазон игроков в турнирах
+}
 
