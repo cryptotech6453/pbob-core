7032b48d9cc4fef494397648986fa72db8cc2c86
cryptotech6453 <dl6453@gmail.com>
2025-03-08 16:20:26 +0200

    Update PlayerNFT.rs

--- PlayerNFT.rs
+++ PlayerNFT.rs
@@ -1,4 +1,14 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct PlayerNFT {
+    pub owner: String,   // Владелец NFT
+    pub abi: u32,        // Средний бай-ин турниров
+    pub games: u32,      // Количество сыгранных турниров
+    pub games_per_month: u32, // Среднее количество турниров в месяц
+    pub roi: f64,        // ROI игрока
+    pub dollars_per_tournament: f64, // Средний выигрыш с турнира
+    pub afs: u32,        // Среднее количество участников в турнирах
+}
 
